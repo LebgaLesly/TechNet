2d62f73c517448b80cffed765c23800f45c3b1d8
LebgaLesly <lebgalesly1@gmail.com>
2023-10-16 17:10:46 -0700

    troubleshooting

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const userRoutes = require('./Routes/UserRoutes')
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://tech-net-f.onrender.com/']
+  origin: ['http://localhost:3000']
 }));
 
 //connecting to database
